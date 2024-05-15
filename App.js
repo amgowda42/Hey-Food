bc5dc0f7e091a547f5311f07f0fe3dfd9fd3c630
Annappa Gowda <annappag2020@gmail.com>
2024-05-15 18:03:19 +0530

    static res-card done

--- App.js
+++ App.js
@@ -19,12 +19,52 @@ const Header =() => {
         </div>
       </div>
     );
+};
+
+const RestaurantCard = () => {
+    return (
+      <div className="res-card">
+        <img className='res-logo'
+          src="https://images.jdmagicbox.com/comp/bangalore/u9/080pxx80.xx80.190514210113.p6u9/catalogue/meghana-foods-sahakara-nagar-bangalore-biryani-restaurants-1xwaklssvg.jpg"
+          alt="res-img"
+        />
+        <h3>Meghana Foods</h3>
+        <h4>Biriyani,south indian,</h4>
+        <h4>4 stars</h4>
+        <h5>36 minuts</h5>
+      </div>
+    );
+};
+
+const Body =() => {
+    return (
+      <div className="body">
+        <div className="search">Search</div>
+        <div className="res-container">
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+        </div>
+      </div>
+    );
 }
 
 const  App = () => {
-    return(
-<Header />
-    )
+    return (
+      <>
+        <Header />
+        <Body />
+      </>
+    );
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
