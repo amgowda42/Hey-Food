444249267a41f707ad79b93a856c0c19696ae2d1
Annappa Gowda <annappag2020@gmail.com>
2024-05-17 20:27:47 +0530

    added usestate hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,32 @@
 import { resList } from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
+
 const Body = () => {
+  const [listOfRestuarant, setListOfRestuarant] = useState(resList);
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestuarant.filter(
+              (res) => res.info.avgRating > 4.3
+            );
+            setListOfRestuarant(filteredList);
+          }}
+        >
+          Top restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restuarant, key) => (
-          <RestaurantCard key={restuarant.info.id} resData={restuarant} />
+        {listOfRestuarant.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
     </div>
   );
 };
+
 export default Body;
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -2,7 +2,8 @@ import { RES_IMG_URL } from "../utils/constants";
 
 const RestaurantCard = (props) => {
   const { resData } = props;
-  const { name, cuisines, cloudinaryImageId, locality } = resData?.info;
+  const { name, cuisines, cloudinaryImageId, locality, avgRating } =
+    resData?.info;
   const { deliveryTime } = resData.info.sla;
   return (
     <div className="res-card">
@@ -13,6 +14,7 @@ const RestaurantCard = (props) => {
       />
       <h3>{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating}</h4>
       <h4>{deliveryTime} min</h4>
       <h4>{locality}</h4>
     </div>
