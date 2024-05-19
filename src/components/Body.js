3deb8e014ff5283fae58895c0d3b67ff70b369a8
Annappa Gowda <annappag2020@gmail.com>
2024-05-19 16:46:03 +0530

    shimmer ui added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,24 +1,27 @@
-
 import RestaurantCard from "./RestaurantCard";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
   const [listOfRestuarant, setListOfRestuarant] = useState([]);
 
   useEffect(() => {
     fetchData();
-  },[]);
- 
+  }, []);
 
-  const fetchData = async() => {
+  const fetchData = async () => {
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
-    const json =await  data.json();
-    console.log(json);
-    setListOfRestuarant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+    const json = await data.json();
+    setListOfRestuarant(
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
   };
 
+  if (listOfRestuarant.length === 0) {
+    return <Shimmer />;
+  }
 
   return (
     <div className="body">
