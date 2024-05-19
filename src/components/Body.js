d901c6457be5934cfda3871b246619a5a0b66992
Annappa Gowda <annappag2020@gmail.com>
2024-05-19 16:04:12 +0530

    live api has been addded

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,24 @@
-import { resList } from "../utils/mockData";
+
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useState, useEffect} from "react";
 
 const Body = () => {
-  const [listOfRestuarant, setListOfRestuarant] = useState(resList);
+  const [listOfRestuarant, setListOfRestuarant] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  },[]);
+ 
+
+  const fetchData = async() => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json =await  data.json();
+    console.log(json);
+    setListOfRestuarant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+  };
+
 
   return (
     <div className="body">
