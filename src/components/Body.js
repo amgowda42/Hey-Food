9a58d8f1bc148a4947477f83cfe92eda34256803
Annappa Gowda <annappag2020@gmail.com>
2024-05-20 16:19:35 +0530

    optimized more code

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
 import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
+import { SWIGGY_API } from "../utils/constants";
 
 const Body = () => {
   const [listOfRestuarant, setListOfRestuarant] = useState([]);
@@ -12,10 +13,11 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    const data = await fetch(SWIGGY_API
     );
+    console.log(data);
     const json = await data.json();
+    console.log(json);
     setListOfRestuarant(
       json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
