d133abd13100aab5716f07c9940d0f771ffe8b85
Annappa Gowda <annappg2020@gmail.com>
2024-05-26 14:30:38 +0530

    Aplied Class Based Components

--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,15 @@
+import { useState } from "react";
+
 const User = (props) => {
     const {name , dis , email} = props;
-    console.log(props); 
+    console.log(props);
+    const[count , setCount] = useState(0)
 
     return (
         <div className="user-card">
+            <h1>count:{count}</h1><button onClick={()=>{
+                setCount(count+1);
+            }}>+</button>
             <h1>Name:{name}</h1>
             <h2>Discription:{dis}</h2>
             <h3>Contact:{email}</h3>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -2,14 +2,27 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      count: 0,
+      count2:2,
+      count3:3
+    };
 
     console.log(props);
   }
 
   render() {
     const { name, dis, email } = this.props;
+    const { count,count2 } = this.state;
     return (
       <div className="user-card">
+        <h1>count : {count}</h1>
+        <h2>count2:{count2}</h2>
+        <button onClick={() =>{
+          this.setState({ count:this.state.count+1,
+          count2:this.state.count2+1
+          });
+        }}> Incriment</button>
         <h1>Name:{name}</h1>
         <h2>Discription:{dis}</h2>
         <h3>Contact:{email}</h3>
