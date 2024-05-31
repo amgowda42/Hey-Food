fc86de315d2b9efccc110c4b9d5b676add3f2ff3
Annappa Gowda <annappg2020@gmail.com>
2024-05-31 23:56:42 +0530

    Lazy Loading added

---
+++ src/components/Grocery.js
@@ -0,0 +1,4 @@
+const Grocery = () => {
+  return <h1>Grocery Which is again large application with lots of content</h1>;
+};
+export default Grocery;
--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -10,8 +10,7 @@ const useRestaurantMenu = (resId) => {
   const fetchMenu = async () => {
     const data = await fetch(MENU_API + resId);
     const json = await data.json();
-    setResInfo(json.data);
-    console.log(json.data);
+    setResInfo(json.data); // console.log(json.data);
   };
   return resInfo;
 };
