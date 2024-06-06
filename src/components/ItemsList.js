b72ca08c3d911eb0ecde54394eb3d7b58752c0ca
Annappa Gowda <annappg2020@gmail.com>
2024-06-06 11:52:38 +0530

    updates

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,3 +1,5 @@
+import { RES_IMG_URL } from "../utils/constants";
+
 const ItemsList = ({ items }) => {
   console.log(items);
 
@@ -6,21 +8,30 @@ const ItemsList = ({ items }) => {
       {items.map((item) => (
         <div
           key={item?.card?.info?.id}
-          className="m-2 p-2 border-b-2 border-orange-600 text-start"
+          className="m-2 p-2 border-b-2 border-orange-600 text-start flex justify-between items-center"
         >
           <div>
-            <span className="font-semibold text-base">
-              {item?.card?.info?.name}
-            </span>
-            <br />
-            <span className="font-semibold flex items-center justify-start text-base">
-              Rs.
-              {item?.card?.info?.price / 100 ||
-                item?.card?.info?.defaultPrice / 100}
-            </span>
+            <div>
+              <span className="font-semibold text-base">
+                {item?.card?.info?.name}
+              </span>
+              <br />
+              <span className="font-semibold flex items-center justify-start text-base text-red-700">
+                Rs.
+                {item?.card?.info?.price / 100 || 
+                  item?.card?.info?.defaultPrice / 100}
+              </span>
+            </div>
+            <div className="my-2">
+              <p className="text-sm">{item?.card?.info?.description}</p>
+            </div>
           </div>
-          <div>
-            <p className="text-sm">{item?.card?.info?.description}</p>
+          <div className="max-w-32 mx-3  rounded-md border-red-300 border-2">
+            <img
+              src={RES_IMG_URL + item?.card?.info?.imageId}
+              alt="item image"
+              className="rounded-md"
+            />
           </div>
         </div>
       ))}
--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -5,7 +5,7 @@ const RestaurantCategory = ({ data }) => {
     <div className="font-serif">
       <div className="w-6/12 bg-gray-50 shadow-lg mx-auto my-6 ">
         <div className="flex justify-between">
-          <span className="font-bold text-lg text-sky-950">
+          <span className="font-bold text-xl text-green-500">
             {data?.title} ({data?.itemCards?.length})
           </span>
           <span>🔽</span>
