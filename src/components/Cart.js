746c3e625b173a4f44d9a357ded66d1b70399135
Annappa Gowda <annappg2020@gmail.com>
2024-06-10 11:09:04 +0530

    updates

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import ItemsList from "./ItemsList";
-import { cartClear } from "../utils/cartSlice";
+import { cartClear, removeItem } from "../utils/cartSlice";
 
 const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
@@ -8,16 +8,29 @@ const Cart = () => {
   handleCartClear = () => {
     dispatch(cartClear());
   };
+  handleRemoveClear = () => {
+    dispatch(removeItem());
+  };
   return (
-    <div className="flex flex-col justify-center items-center w-5/12 mx-auto">
+    <div className="flex flex-col justify-center items-center w-5/12 mx-auto my-6">
       <h1 className="font-bold text-lime-500 text-2xl">Cart</h1>
-      <button
-        className="bg-slate-950 text-white py-1 px-3 "
-        onClick={handleCartClear}
-      >
-        Clear
-      </button>
-      {cartItems.length === 0 && <h1>Your Cart Is emty Add Some Food to Cart</h1>}
+      <div className="flex gap-10 my-5">
+        <button
+          className="bg-red-700 text-white py-1 px-6 rounded-md text-xl"
+          onClick={handleCartClear}
+        >
+          Clear
+        </button>
+        <button
+          className="bg-amber-600 text-white py-1 px-4 rounded-md text-xl"
+          onClick={handleRemoveClear}
+        >
+          Remove
+        </button>
+      </div>
+      {cartItems.length === 0 && (
+        <h1>Your Cart Is emty Add Some Food to Cart</h1>
+      )}
       <ItemsList items={cartItems} />
     </div>
   );
